9dabd3d2a0a14438d32be058cc451fb3b5b157e3
jdcar <jdcarlstone@gmail.com>
2021-03-26 09:23:57 -0500

    Add react bootstrap, components, pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
+import Home from './pages/Home'
+import Directory from './pages/Directory'
 import './App.css';
+
+import Navbar from './components/Jumbotron';
 
 function App() {
   return (
-    <div>REact page</div>
+    <Router>
+      <Navbar />
+      <div className="container">
+        <Switch>
+          <Route path="/directory">
+            <Directory />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+    // <div>REact page</div>
   );
 }
 
---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Discription from '../components/Discription'
+
+const Home = () => {
+    return (
+    <div>
+        <Discription />
+    </div>
+    )
+}
+
+export default Home
